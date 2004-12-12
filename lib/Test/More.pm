9b0271693e446bbf8fdb8fd25c7a69949c85100c
Michael G Schwern <schwern@pobox.com>
2004-12-12 00:04:08 +0000

    Version 0.53_01

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -18,7 +18,7 @@ sub _carp {
 
 require Exporter;
 use vars qw($VERSION @ISA @EXPORT %EXPORT_TAGS $TODO);
-$VERSION = '0.53';
+$VERSION = '0.53_01';
 $VERSION = eval $VERSION;    # make the alpha version come out as a number
 
 @ISA    = qw(Exporter);
