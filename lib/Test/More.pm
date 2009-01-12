ec64ba77d4c19032f71a0d8aa0aaceb87d1453af
Michael G Schwern <schwern@pobox.com>
2009-01-12 03:57:10 +0000

    Merge commit 'trunk' into HEAD
    Conflicts:
    	Makefile.PL
    	lib/Test/Builder2/History.pm
    	lib/Test/Simple.pm

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -18,7 +18,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '0.84';
+our $VERSION = '0.86';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Builder::Module;
