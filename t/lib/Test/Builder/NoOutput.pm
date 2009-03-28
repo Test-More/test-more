525c1202ba1b224669bcf692b2805182dd700c0f
Michael G. Schwern <schwern@pobox.com>
2009-03-28 12:51:03 +0000

    Pull Test::Builder::NoOutput::Tee out into its own library
    Test::Builder::Output will make use if it.

--- t/lib/Test/Builder/NoOutput.pm
+++ t/lib/Test/Builder/NoOutput.pm
@@ -60,9 +60,10 @@ sub create {
     );
     $self->{_outputs} = \%outputs;
 
-    tie *OUT,  "Test::Builder::NoOutput::Tee", \$outputs{all}, \$outputs{out};
-    tie *ERR,  "Test::Builder::NoOutput::Tee", \$outputs{all}, \$outputs{err};
-    tie *TODO, "Test::Builder::NoOutput::Tee", \$outputs{all}, \$outputs{todo};
+    require Test::Builder::Tee;
+    tie *OUT,  "Test::Builder::Tee", \$outputs{all}, \$outputs{out};
+    tie *ERR,  "Test::Builder::Tee", \$outputs{all}, \$outputs{err};
+    tie *TODO, "Test::Builder::Tee", \$outputs{all}, \$outputs{todo};
 
     $self->output(*OUT);
     $self->failure_output(*ERR);
@@ -86,37 +87,6 @@ sub read {
     }
 
     return $out;
-}
-
-
-package Test::Builder::NoOutput::Tee;
-
-# A cheap implementation of IO::Tee.
-
-sub TIEHANDLE {
-    my($class, @refs) = @_;
-
-    my @fhs;
-    for my $ref (@refs) {
-        open my $fh, ">>", $ref or die $!;
-        push @fhs, $fh;
-    }
-
-    my $self = [@fhs];
-    return bless $self, $class;
-}
-
-sub PRINT {
-    my $self = shift;
-
-    print $_ @_ for @$self;
-}
-
-sub PRINTF {
-    my $self   = shift;
-    my $format = shift;
-
-    printf $_ @_ for @$self;
 }
 
 1;
---
+++ t/lib/Test/Builder/Tee.pm
@@ -0,0 +1,31 @@
+package Test::Builder::Tee;
+
+# A cheap implementation of IO::Tee.
+
+sub TIEHANDLE {
+    my($class, @refs) = @_;
+
+    my @fhs;
+    for my $ref (@refs) {
+        open my $fh, ">>", $ref or die $!;
+        push @fhs, $fh;
+    }
+
+    my $self = [@fhs];
+    return bless $self, $class;
+}
+
+sub PRINT {
+    my $self = shift;
+
+    print $_ @_ for @$self;
+}
+
+sub PRINTF {
+    my $self   = shift;
+    my $format = shift;
+
+    printf $_ @_ for @$self;
+}
+
+1;
