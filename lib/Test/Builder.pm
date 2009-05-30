28eac0f36fd2d67c7ab86e421636b5d0f6052404
Michael G. Schwern <schwern@pobox.com>
2009-05-30 12:32:53 -0700

    Version 0.88

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.87_03';
+our $VERSION = '0.88';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 BEGIN {
--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -17,7 +17,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '0.87_03';
+our $VERSION = '0.88';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Builder::Module;
