b3d0d75ef37d009257f3d1db153fc41146a148a9
Michael G. Schwern <schwern@pobox.com>
2010-05-19 15:56:33 -0700

    Version 0.95_02

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -17,7 +17,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '0.95_01';
+our $VERSION = '0.95_02';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Builder::Module;
