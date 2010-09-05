f39819c7ddfb1ab24d2cfa278d6a68aeb127fd4d
Michael G. Schwern <schwern@pobox.com>
2010-09-05 01:16:06 -0700

    Document the custom types

--- lib/Test/Builder2/Types.pm
+++ lib/Test/Builder2/Types.pm
@@ -3,11 +3,38 @@ package Test::Builder2::Types;
 use Test::Builder2::Mouse::Util qw(load_class);
 use Test::Builder2::Mouse::Util::TypeConstraints;
 
+
+=head1 NAME
+
+Test::Builder2::Types - Mouse types used by Test::Builder2
+
+=head1 SYNOPSIS
+
+    use Test::Builder2::Types;
+
+=head1 DESCRIPTION
+
+This defines custom Mouse types used by Test::Builder2.
+
+=head2 Types
+
+=head3 Test::Buidler2::Positive_Int
+
+An integer greater than or equal to zero.
+
+=cut
+
 subtype 'Test::Builder2::Positive_Int' => (
     as 'Int',
     where { $_ >= 0 },
 );
 
+
+=head3 Test::Builder2::LoadableClass
+
+A class name.  It will be loaded.
+
+=cut
 
 subtype 'Test::Builder2::LoadableClass', as 'ClassName';
 coerce 'Test::Builder2::LoadableClass', from 'Str', via { load_class($_); $_ };
