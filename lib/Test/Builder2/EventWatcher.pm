a8d54b9c52e64cd7af58753ef2c225f768675ed6
Michael G. Schwern <schwern@pobox.com>
2010-12-07 11:37:23 +0930

    The EventWatcher!
    Nothing uses it yet.  Coming up.

---
+++ lib/Test/Builder2/EventWatcher.pm
@@ -0,0 +1,73 @@
+package Test::Builder2::EventWatcher;
+
+use Test::Builder2::Mouse::Role;
+
+requires qw(accept_event);
+
+no Test::Builder2::Mouse::Role;
+
+
+=head1 NAME
+
+Test::Builder2::EventWatcher - A role which watches events and results
+
+=head1 SYNOPSIS
+
+  package My::EventWatcher;
+
+  use Test::Builder2::Mouse;
+  with "Test::Builder2::EventWatcher";
+
+  sub accept_event  { ... }
+
+  no Test::Builder2::Mouse;
+
+
+=head1 DESCRIPTION
+
+An EventWatcher is made known to an EventCoordinator which gives it
+Events and Results to do whatever it wants with.  EventWatchers can be
+used to record events for future use (such as
+L<Test::Builder2::History>), to take an action like producing output
+(such as L<Test::Builder2::Formatter>) or even modifying the event
+itself.
+
+=head1 METHODS
+
+=head2 Required Methods
+
+When writing an EventWatcher you must supply these methods.
+
+=head3 accept_event
+
+    $event_watcher->accept_event($event);
+
+An EventWatcher will be handed Event objects to do whatever it wants
+to with via this method.
+
+C<accept_event> is allowed to alter the $event.  Be aware those
+changes B<will> be visible to other EventWatchers.
+
+You must implement this method.
+
+=head2 Supplied Methods
+
+The EventWatcher role supplies these methods.
+
+=head3 accept_result
+
+    $event_watcher->accept_result($result);
+
+Just like L<accept_event> except it handles Results (which are a
+special type of Event).  It is there to make special case handling of
+Results a little more convenient.
+
+The provided C<accept_result> simply passes through to C<accept_event>.
+
+=cut
+
+sub accept_result {
+    shift->accept_event(@_);
+}
+
+1;
---
+++ t/Builder2/EventWatcher.t
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+BEGIN { require 't/test.pl'; }
+
+{
+    package My::Watcher;
+
+    use Test::Builder2::Mouse;
+    with "Test::Builder2::EventWatcher";
+
+    has events =>
+      is                => 'rw',
+      isa               => 'ArrayRef',
+      default           => sub { [] },
+    ;
+
+    sub accept_event {
+        my $self = shift;
+        push @{$self->events}, @_;
+    }
+}
+
+
+note "accept_result() passes to accept_event()"; {
+    my $ew = My::Watcher->new;
+
+    $ew->accept_event({ foo => 42 });
+    $ew->accept_result({ bar => 23 });
+
+    is @{$ew->events},          2,       "events accepted";
+    ok eq_hash($ew->events->[0], { foo => 42 }), "accept_event";
+    ok eq_hash($ew->events->[1], { bar => 23 }), "accept_result pass through";
+}
+
+
+note "can't make a watcher without accept_event()"; {
+    ok !eval {
+        package My::Bad::Watcher;
+        use Test::Builder2::Mouse;
+        with "Test::Builder2::EventWatcher";
+        sub accept_result {}
+    };
+    like $@, q['Test::Builder2::EventWatcher' requires the method 'accept_event'];
+}
+
+
+done_testing();
