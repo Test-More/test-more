524a2333859c1b558760f4dfe6f272a21f535c69
Michael G. Schwern <schwern@pobox.com>
2011-01-08 17:42:50 +1100

    Update the URL to the license to one that works.

--- lib/Test/Builder2.pm
+++ lib/Test/Builder2.pm
@@ -384,7 +384,7 @@ Copyright 2008-2010 by Michael G Schwern E<lt>schwernE<0x40>pobox.comE<gt>.
 This program is free software; you can redistribute it and/or 
 modify it under the same terms as Perl itself.
 
-See L<http://www.perl.com/perl/misc/Artistic.html>
+See L<http://dev.perl.org/licenses/artistic.html>
 
 
 =head1 SEE ALSO
