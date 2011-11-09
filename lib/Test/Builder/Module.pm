fff6727e80bd63fcce48ef5cd0d532bb235b5653
Michael G. Schwern <schwern@pobox.com>
2011-11-08 17:14:19 -0800

    Version 0.98_01

--- lib/Test/Builder/Module.pm
+++ lib/Test/Builder/Module.pm
@@ -7,7 +7,7 @@ use Test::Builder 0.98;
 require Exporter;
 our @ISA = qw(Exporter);
 
-our $VERSION = '0.98';
+our $VERSION = '0.98_01';
 $VERSION = eval $VERSION;      ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 
--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -17,7 +17,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '0.98';
+our $VERSION = '0.98_01';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Builder::Module 0.98;
