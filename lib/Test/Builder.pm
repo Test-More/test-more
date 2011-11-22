c75a5282b5cb7624f28d1a102c85120e025fd733
dunsmoreb <dunsmoreb@gmail.com>
2011-11-21 19:50:30 -0600

    Added documentation for is_subtest.

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -201,6 +201,13 @@ sub name {
     $self->croak("The internals of subtests were redesigned, name() no longer exists.");
 }
 
+=item B<is_subtest>
+
+	$builder->is_subtest;
+
+Whether the Test::Builder is a subtest.
+
+=cut
 
 sub is_subtest {
 	$self->history->is_subtest
