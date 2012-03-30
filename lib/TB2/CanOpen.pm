1d1ac7becf803d2d7755e3b133a7fe9a238b3feb
Michael G. Schwern <schwern@pobox.com>
2012-03-30 15:13:11 +0200

    Write a role to provide a safe open method, TB2::CanOpen
    For #268

---
+++ lib/TB2/CanOpen.pm
@@ -0,0 +1,37 @@
+package TB2::CanOpen;
+
+use TB2::Mouse ();
+use TB2::Mouse::Role;
+
+our $VERSION = '1.005000_004';
+$VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
+
+
+=begin private
+
+=head3 open
+
+    my $fh = $obj->open($mode, $file);
+
+Works like L<perlfunc/open> but ensures global variables like C<$!>
+and C<$@> are not blown over.
+
+If the open fails, it will throw an exception containing C<$!>.
+
+=cut
+
+sub open {
+    my $self = shift;
+    my($mode, $file) = @_;
+
+    local($!, $@);
+    open my $fh, $mode, $file or die "$!\n";
+
+    return $fh;
+}
+
+=end private
+
+=cut
+
+1;
