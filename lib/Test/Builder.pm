59b0aa9e759a924ff96dd4ae3297402ed9805051
Michael G. Schwern <schwern@pobox.com>
2012-06-21 13:07:46 -0700

    Version 0.98_03

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.98_02';
+our $VERSION = '0.98_03';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 BEGIN {
