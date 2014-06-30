013ceb1892315f67af2c42ec69d10a06eb5cc5ba
Chad Granum <chad.granum@dreamhost.com>
2014-06-29 22:01:52 -0700

    Split up provider import into smaller bits

--- lib/Test/Builder/Provider.pm
+++ lib/Test/Builder/Provider.pm
@@ -14,62 +14,68 @@ my %SIG_MAP = (
     '&' => 'CODE',
 );
 
-my $id = 1;
+my $ID = 1;
+
 sub import {
     my $class = shift;
-    my @sym_list = @_;
     my $caller = caller;
-    my $root = $caller;
+
+    $class->export_into($caller, @_);
+}
+
+sub export_into {
+    my $class = shift;
+    my ($dest, @sym_list) = @_;
 
     my $meta = {refs => {}, attrs => {}};
     my %subs;
 
     $subs{TB_PROVIDER_META} = sub { $meta };
 
-    # to help transition legacy
-    my $builder = sub {
-        my $trace = Test::Builder->trace_test;
+    $subs{TB}      = \&find_builder;
+    $subs{builder} = \&find_builder;
+    $subs{anoint}  = \&anoint;
+    $subs{import}  = \&provider_import;
+    $subs{provide} = $class->_build_provide($dest, $meta);
+    $subs{export}  = $class->_build_export($dest, $meta);
 
-        if ($trace && $trace->{report}) {
-            my $pkg = $trace->{package};
-            return $pkg->TB_INSTANCE
-                if $pkg && $pkg->can('TB_INSTANCE');
-        }
+    $subs{provide_nests} = sub { $subs{provide}->($_,    undef, nest => 1) for @_ };
+    $subs{provide_nest}  = sub { $subs{provide}->($_[0], $_[1], nest => 1)        };
+    $subs{gives}         = sub { $subs{provide}->($_,    undef, give => 1) for @_ };
+    $subs{give}          = sub { $subs{provide}->($_[0], $_[1], give => 1)        };
+    $subs{provides}      = sub { $subs{provide}->($_)                      for @_ };
 
-        return Test::Builder->new;
-    };
+    @sym_list = keys %subs unless @sym_list;
 
-    $subs{builder} = $builder;
-    $subs{TB}      = $builder;
+    my %seen;
+    for my $name (grep { !$seen{$_}++ } @sym_list, qw/TB_PROVIDER_META/) {
+        no strict 'refs';
+        my $ref = $subs{$name} || $class->can($name);
+        croak "$class does not export '$name'" unless $ref;
+        *{"$dest\::$name"} = $ref ;
+    }
 
-    $subs{anoint} = sub { Test::Builder->anoint($_[1], $_[0]) };
+    1;
+}
 
-    $subs{provide_nests} = sub {
-        for my $provide (@_) {
-            $subs{provide}->($provide, undef, nest => 1);
-        }
-    };
-    $subs{provide_nest} = sub {
-        $subs{provide}->($_[0], $_[1], nest => 1);
-    };
+sub _build_provide {
+    my $class = shift;
+    my ($dest, $meta) = @_;
 
-    $subs{gives}    = sub { $subs{provide}->($_, undef, give => 1) for @_ };
-    $subs{give}     = sub { $subs{provide}->(@_, give => 1 ) };
-    $subs{provides} = sub { $subs{provide}->($_) for @_ };
-    $subs{provide}  = sub {
+    return sub {
         my ($name, $ref, %params) = @_;
 
-        croak "$caller already provides or gives '$name'"
+        croak "$dest already provides or gives '$name'"
             if $meta->{attrs}->{$name};
 
         croak "The second argument to provide() must be a ref, got: $ref"
             if $ref && !ref $ref;
 
-        $ref ||= $caller->can($name);
-        croak "$caller has no sub named '$name', and no ref was given"
+        $ref ||= $dest->can($name);
+        croak "$dest has no sub named '$name', and no ref was given"
             unless $ref;
 
-        my $attrs = {%params, package => $caller, name => $name};
+        my $attrs = {%params, package => $dest, name => $name};
         $meta->{attrs}->{$name} = $attrs;
 
         # If this is just giving, or not a coderef
@@ -86,7 +92,7 @@ sub import {
             # Insert an anonymous sub, and use a trick to make caller() think its
             # name is this string, which tells us how to find the thing that was
             # actually called.
-            my $globname = __PACKAGE__ . '::__ANON' . ($id++) . '__';
+            my $globname = __PACKAGE__ . '::__ANON' . ($ID++) . '__';
 
             my $code = sub {
                 no warnings 'once';
@@ -107,8 +113,13 @@ sub import {
             *$globname = $attrs;
         }
     };
+}
 
-    $subs{export} = sub {
+sub _build_export {
+    my $class = shift;
+    my ($dest, $meta) = @_;
+
+    return sub {
         my $class = shift;
         my ($caller, @args) = @_;
 
@@ -128,7 +139,7 @@ sub import {
             %export    = map {($_ => 1)} @{"$class\::EXPORT"};
             %export_ok = map {($_ => 1)} @{"$class\::EXPORT_OK"}, @{"$class\::EXPORT"};
         }
-        warn "package '$class' uses \@EXPORT and/or \@EXPORT_OK, this is deprecated since '$root' is no longer a subclass of 'Exporter'\n"
+        warn "package '$class' uses \@EXPORT and/or \@EXPORT_OK, this is deprecated since '$dest' is no longer a subclass of 'Exporter'\n"
             if keys %export_ok;
 
         unless(@list) {
@@ -157,32 +168,32 @@ sub import {
             }
         }
     };
+}
 
-    $subs{import} = sub {
-        my $class = shift;
-        my $caller = caller;
+sub provider_import {
+    my $class = shift;
+    my $caller = caller;
 
-        $class->anoint($caller);
-
-        $class->before_import(\@_) if $class->can('before_import');
-
-        $class->export($caller, @_);
-
-        $class->after_import(@_) if $class->can('after_import');
-
-        1;
-    };
-
-    @sym_list = keys %subs unless @sym_list;
-
-    for my $name (@sym_list) {
-        no strict 'refs';
-        my $ref = $subs{$name} || $class->can($name);
-        croak "$class does not export '$name'" unless $ref;
-        *{"$caller\::$name"} = $ref ;
-    }
+    $class->anoint($caller);
+    $class->before_import(\@_) if $class->can('before_import');
+    $class->export($caller, @_);
+    $class->after_import(@_)   if $class->can('after_import');
 
     1;
 }
+
+sub find_builder {
+    my $trace = Test::Builder->trace_test;
+
+    if ($trace && $trace->{report}) {
+        my $pkg = $trace->{package};
+        return $pkg->TB_INSTANCE
+            if $pkg && $pkg->can('TB_INSTANCE');
+    }
+
+    return Test::Builder->new;
+}
+
+sub anoint { Test::Builder->anoint($_[1], $_[0]) };
 
 1;
