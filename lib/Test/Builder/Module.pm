f14d3da43a6f63a9189913cb44922a9ddc1e7127
Chad Granum <chad.granum@dreamhost.com>
2014-08-01 15:32:04 -0700

    Version Bump: alpha 19

--- lib/Test/Builder/Module.pm
+++ lib/Test/Builder/Module.pm
@@ -7,7 +7,7 @@ use Test::Builder 0.99;
 require Exporter;
 our @ISA = qw(Exporter);
 
-our $VERSION = '1.301001_018';
+our $VERSION = '1.301001_019';
 $VERSION = eval $VERSION;      ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 
--- lib/Test/Simple.pm
+++ lib/Test/Simple.pm
@@ -4,7 +4,7 @@ use 5.008001;
 
 use strict;
 
-our $VERSION = '1.301001_018';
+our $VERSION = '1.301001_019';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Builder::Provider;
