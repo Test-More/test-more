e124674e88d11b6222c4132140f749c204ecc741
Chad Granum <chad.granum@dreamhost.com>
2014-08-10 12:50:54 -0700

    Alpha 27

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -19,7 +19,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '1.301001_026';
+our $VERSION = '1.301001_027';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 our $TODO;
--- lib/ok.pm
+++ lib/ok.pm
@@ -5,7 +5,7 @@ use warnings;
 use Test::More ();
 use Carp qw/croak/;
 
-our $VERSION = '1.301001_026';
+our $VERSION = '1.301001_027';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 sub import {
