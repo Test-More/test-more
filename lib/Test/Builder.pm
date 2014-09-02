0ae48ef5ef3149a9f148cd000786c08080d6bd94
Chad Granum <chad.granum@dreamhost.com>
2014-09-02 14:39:05 -0700

    Version bump again for consistency

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '1.001005';
+our $VERSION = '1.001006';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 BEGIN {
