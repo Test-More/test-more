34c34737c3bcd4b8429d40eb92e937e73b3d48a3
Chad Granum <chad.granum@dreamhost.com>
2014-09-20 13:00:47 -0700

    Version Bump

--- lib/Test/Builder/Module.pm
+++ lib/Test/Builder/Module.pm
@@ -7,7 +7,7 @@ use Test::Builder 0.99;
 require Exporter;
 our @ISA = qw(Exporter);
 
-our $VERSION = '1.301001_042';
+our $VERSION = '1.301001_043';
 $VERSION = eval $VERSION;      ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 
--- lib/Test/Stream.pm
+++ lib/Test/Stream.pm
@@ -2,7 +2,7 @@ package Test::Stream;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_042';
+our $VERSION = '1.301001_043';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Stream::Threads;
