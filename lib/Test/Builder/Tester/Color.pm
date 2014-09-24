b3a809c50c3b077302fdd0c59b15eb6070048295
Chad Granum <chad.granum@dreamhost.com>
2014-09-23 19:31:53 -0700

    Version Bump

--- lib/Test/Builder/Tester/Color.pm
+++ lib/Test/Builder/Tester/Color.pm
@@ -1,7 +1,7 @@
 package Test::Builder::Tester::Color;
 
 use strict;
-our $VERSION = '1.301001_046';
+our $VERSION = '1.301001_047';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 require Test::Builder::Tester;
