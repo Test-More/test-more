1b6165d1d9456f522867621f5ee2ecc8ac0bdf7a
Chad Granum <chad.granum@dreamhost.com>
2014-10-15 15:19:34 -0700

    Version Bump

--- lib/Test/Stream.pm
+++ lib/Test/Stream.pm
@@ -2,7 +2,7 @@ package Test::Stream;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_057';
+our $VERSION = '1.301001_058';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Stream::Threads;
