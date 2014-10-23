20d963012e4f592612a4fe55925f0c8a35f5d6df
Chad Granum <chad.granum@dreamhost.com>
2014-10-23 15:30:00 -0700

    Version Bump

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -4,7 +4,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_062';
+our $VERSION = '1.301001_063';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Stream::Carp qw/croak carp/;
--- lib/Test/Stream.pm
+++ lib/Test/Stream.pm
@@ -2,7 +2,7 @@ package Test::Stream;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_062';
+our $VERSION = '1.301001_063';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Stream::Threads;
