e618e7b5f08e558f089836de8110807a180a5df1
Chad Granum <chad.granum@dreamhost.com>
2014-10-29 15:25:07 -0700

    Profiling tools modifications

--- profiling/long-run.pl
+++ profiling/long-run.pl
@@ -12,40 +12,36 @@ my $tb = Test::Builder->new;
 
 for (1 .. 1000) {
     print "Loop: $_\n";
-    ok($_, "pass") for 1 .. 100;
 
-    is(1, 1, "is pass");
+    run();
+
+    subtest foo => sub {
+        run()
+    }
+}
+
+sub run {
+    ok($_, "pass") for 1 .. 10;
+
+    is(1, 1, "is pass") for 1 .. 10;
+    is("foo", "foo", "is pass 2") for 1 .. 10;
 
     is_deeply(
-        { a => 1 },
-        { a => 1 },
+        {a => {a => {a => {a => {a => $_}}}}},
+        {a => {a => {a => {a => {a => $_}}}}},
         'hash pass',
-    );
+    ) for 1 .. 10;
 
-    like( 'aaa', qr/a/, "Like pass" );
+    like( 'aaa', qr/a/, "Like pass" ) for 1 .. 10;
 
     TODO: {
         local $TODO = "Blah";
         ok(1, "todo pass");
     }
 
-    subtest foo => sub {
-        ok($_, "sub pass") for 1 .. 100;
-
-        is(1, 1, "sub is pass");
-
-        is_deeply(
-            { a => 1 },
-            { a => 1 },
-            'sub hash pass',
-        );
-
-        like( 'aaa', qr/a/, "sub Like pass" );
-
-        TODO: {
-            local $TODO = "Blah";
-            ok(1, "sub todo pass");
-        }
+    SKIP: {
+        skip 'foo' => 5;
+        die "oops";
     }
 }
 
