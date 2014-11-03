fbbecd77c30bcf887cec572a2453a39468188b22
Chad Granum <chad.granum@dreamhost.com>
2014-11-02 22:31:08 -0800

    Version Bump 1.001009

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '1.001008';
+our $VERSION = '1.001009';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 BEGIN {
