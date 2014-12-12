c73996a232b473c7eb8dd00cd8a2ee88aa919243
Chad Granum <chad.granum@dreamhost.com>
2014-12-12 15:32:22 -0800

    Version Bump, RC7, (087)

--- lib/Test/Simple.pm
+++ lib/Test/Simple.pm
@@ -5,10 +5,10 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_086';
+our $VERSION = '1.301001_087';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
-use Test::Stream 1.301001_086 '-internal';
+use Test::Stream 1.301001_087 '-internal';
 use Test::Stream::Toolset;
 
 use Test::Stream::Exporter;
