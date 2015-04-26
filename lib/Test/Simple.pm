d3928aa371fc28fea263e4557f7e11a1dd9a56fa
Chad Granum <exodist7@gmail.com>
2015-04-25 20:36:04 -0700

    Version Bump _108

--- lib/Test/Simple.pm
+++ lib/Test/Simple.pm
@@ -5,10 +5,10 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_107';
+our $VERSION = '1.301001_108';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
-use Test::Stream 1.301001_107 ();
+use Test::Stream 1.301001_108 ();
 use Test::Stream::Toolset;
 
 use Test::Builder;
--- lib/Test/Stream.pm
+++ lib/Test/Stream.pm
@@ -2,7 +2,7 @@ package Test::Stream;
 use strict;
 use warnings;
 
-our $VERSION = '1.301001_107';
+our $VERSION = '1.301001_108';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 use Test::Stream::Carp qw/croak confess carp/;
