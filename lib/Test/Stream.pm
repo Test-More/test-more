891451cd4a89f3877422cf891d28eeba4d06f20b
Chad Granum <exodist7@gmail.com>
2015-06-06 14:05:58 -0700

    Version Bump

--- lib/Test/Stream.pm
+++ lib/Test/Stream.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use vars qw/$VERSION/;
 
-$Test::Stream::VERSION = '1.302001';
+$Test::Stream::VERSION = '1.302002';
 $VERSION = eval $VERSION;
 
 use Scalar::Util qw/reftype/;
