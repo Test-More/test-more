d01243718af73a108ae002bfc302f085be5a93d6
Chad Granum <exodist7@gmail.com>
2015-06-23 22:19:04 -0700

    Document CanThread and CanFork

--- lib/Test/CanFork.pm
+++ lib/Test/CanFork.pm
@@ -12,3 +12,110 @@ sub import {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Test::CanFork - Skip a test file unless the system supports forking
+
+=head1 EXPERIMENTAL CODE WARNING
+
+B<This is an experimental release!> Test-Stream, and all its components are
+still in an experimental phase. This dist has been released to cpan in order to
+allow testers and early adopters the chance to write experimental new tools
+with it, or to add experimental support for it into old tools.
+
+B<PLEASE DO NOT COMPLETELY CONVERT OLD TOOLS YET>. This experimental release is
+very likely to see a lot of code churn. API's may break at any time.
+Test-Stream should NOT be depended on by any toolchain level tools until the
+experimental phase is over.
+
+=head1 DESCRIPTION
+
+It is fairly common to write tests that need to fork. Not all systems support
+forking. This library does the hard work of checking if forking is supported on
+the current system. If forking is not supported then this will skip all tests
+and exit true.
+
+=head1 SYNOPSIS
+
+    use Test::CanFork;
+
+    ... Code that forks ...
+
+=head1 EXPLANATION
+
+Checking if the current system supports forking is not simple, here is an
+example of how to do it:
+
+    use Config;
+
+    sub CAN_FORK {
+        return 1 if $Config{d_fork};
+        return 0 unless $^O eq 'MSWin32' || $^O eq 'NetWare';
+        return 0 unless $Config{useithreads};
+        return 0 unless $Config{ccflags} =~ /-DPERL_IMPLICIT_SYS/;
+
+        # Threads are not reliable before 5.008001
+        return 0 unless $] >= 5.008001;
+        return 0 unless $Config{'useithreads'};
+
+        # Devel::Cover currently breaks with threads
+        return 0 if $INC{'Devel/Cover.pm'};
+        return 1;
+    }
+
+Duplicating this non-trivial code in all tests that need to fork is dumb. It is
+easy to forget bits, or get it wrong. On top of these checks you also need to
+tell the harness that no tests should run and why.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Test::CanThread>
+
+Skip the test file if the system does not support threads.
+
+=item L<Test::Stream>
+
+Test::CanFork uses L<Test::Stream> under the hood.
+
+=back
+
+=head1 SOURCE
+
+The source code repository for Test::Stream can be found at
+F<http://github.com/Test-More/Test-Stream/>.
+
+=head1 MAINTAINERS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2015 Chad Granum E<lt>exodist7@gmail.comE<gt>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See F<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
--- lib/Test/CanThread.pm
+++ lib/Test/CanThread.pm
@@ -12,3 +12,105 @@ sub import {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Test::CanThread - Skip a test file unless the system supports threading
+
+=head1 EXPERIMENTAL CODE WARNING
+
+B<This is an experimental release!> Test-Stream, and all its components are
+still in an experimental phase. This dist has been released to cpan in order to
+allow testers and early adopters the chance to write experimental new tools
+with it, or to add experimental support for it into old tools.
+
+B<PLEASE DO NOT COMPLETELY CONVERT OLD TOOLS YET>. This experimental release is
+very likely to see a lot of code churn. API's may break at any time.
+Test-Stream should NOT be depended on by any toolchain level tools until the
+experimental phase is over.
+
+=head1 DESCRIPTION
+
+It is fairly common to write tests that need to use threads. Not all systems
+support threads. This library does the hard work of checking if threading is
+supported on the current system. If threading is not supported then this will
+skip all tests and exit true.
+
+=head1 SYNOPSIS
+
+    use Test::CanThread;
+
+    ... Code that uses threads ...
+
+=head1 EXPLANATION
+
+Checking if the current system supports threading is not simple, here is an
+example of how to do it:
+
+    use Config;
+
+    sub CAN_THREAD {
+        # Threads are not reliable before 5.008001
+        return 0 unless $] >= 5.008001;
+        return 0 unless $Config{'useithreads'};
+
+        # Devel::Cover currently breaks with threads
+        return 0 if $INC{'Devel/Cover.pm'};
+        return 1;
+    }
+
+Duplicating this non-trivial code in all tests that need to use threafs is
+dumb. It is easy to forget bits, or get it wrong. On top of these checks you
+also need to tell the harness that no tests should run and why.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Test::CanFork>
+
+Skip the test file if the system does not support forking.
+
+=item L<Test::Stream>
+
+Test::CanThread uses L<Test::Stream> under the hood.
+
+=back
+
+=head1 SOURCE
+
+The source code repository for Test::Stream can be found at
+F<http://github.com/Test-More/Test-Stream/>.
+
+=head1 MAINTAINERS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2015 Chad Granum E<lt>exodist7@gmail.comE<gt>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See F<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
