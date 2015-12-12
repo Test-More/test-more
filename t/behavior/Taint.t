Test-More/Test-Stream

Update all the tests

t/behavior/Taint.t
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl -T
-use Test::Sync Core;
-
-plan(2);
+use Test::Sync::Tester;
 
 ok(1);
 ok(1);

t/modules/DebugInfo.t
@@ -1,16 +1,17 @@
-use Test::Sync -V1, -Tester;
+use strict;
+use warnings;
+use Test::Sync::Tester;
 use Test::Sync::DebugInfo;
 
 like(
-    dies { 'Test::Sync::DebugInfo'->new() },
+    exception { 'Test::Sync::DebugInfo'->new() },
     qr/Frame is required/,
     "got error"
 );
 
 my $one = 'Test::Sync::DebugInfo'->new(frame => ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo']);
-isa_ok($one, 'Test::Sync::DebugInfo');
-is($one->frame,  ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got frame");
-is([$one->call], ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got call");
+is_deeply($one->frame,  ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got frame");
+is_deeply([$one->call], ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got call");
 is($one->package, 'Foo::Bar',      "Got package");
 is($one->file,    'foo.t',         "Got file");
 is($one->line,    5,               "Got line");
@@ -21,45 +22,20 @@ $one->set_detail("yo momma");
 is($one->trace, "yo momma", "got detail for trace");
 $one->set_detail(undef);
 
-ok(!eval { $one->throw('I died'); 1 }, "threw exception");
-is($@, "I died at foo.t line 5.\n", "got exception");
+is(
+    exception { $one->throw('I died') },
+    "I died at foo.t line 5.\n",
+    "got exception"
+);
 
-my @warnings;
-{
-    local $SIG{__WARN__} = sub { push @warnings => @_ };
-    $one->alert('I cried');
-}
-
-like(
-    warns { $one->alert('I cried') },
-    [ qr/I cried at foo\.t line 5/ ],
+is_deeply(
+    warnings { $one->alert('I cried') },
+    [ "I cried at foo.t line 5.\n" ],
     "alter() warns"
 );
 
 my $snap = $one->snapshot;
-is($snap, $one, "identical");
+is_deeply($snap, $one, "identical");
 ok($snap != $one, "Not the same instance");
-
-
-ok(!$one->_no_diag, "yes diag");
-ok(!$one->_no_fail, "yes fail");
-
-$one->_set_parent_todo(1);
-ok($one->_no_diag, "no diag");
-ok(!$one->_no_fail, "yes fail");
-
-$one->_set_parent_todo(0);
-$one->_set_todo(1);
-ok($one->_no_diag, "no diag");
-ok($one->_no_fail, "no fail");
-
-$one->_set_todo(undef);
-like(
-    warning { $one->set_skip(1) },
-    qr/Use of 'skip' attribute for DebugInfo is deprecated/,
-    "Got expected warning for deprecated 'skip' attribute"
-);
-ok($one->_no_diag, "no diag");
-ok($one->_no_fail, "no fail");
 
 done_testing;

t/modules/Event.t
@@ -1,8 +1,8 @@
-use Test::Sync -V1;
+use strict;
+use warnings;
+use Test::Sync::Tester;
 
 use Test::Sync::Event();
-
-can_ok('Test::Sync::Event', qw/debug nested/);
 
 my $ok = eval { Test::Sync::Event->new(); 1 };
 my $err = $@;
@@ -16,17 +16,12 @@ like($err, qr/No debug info provided/, "Need debug info");
     use Test::Sync::HashBase accessors => [qw/foo bar baz/];
 }
 
-can_ok('My::MockEvent', qw/foo bar baz/);
-isa_ok('My::MockEvent', 'Test::Sync::Event');
+ok(My::MockEvent->can($_), "Added $_ accessor") for qw/foo bar baz/;
 
 my $one = My::MockEvent->new(debug => 'fake');
 
 ok(!$one->causes_fail, "Events do not cause failures by default");
 
 ok(!$one->$_, "$_ is false by default") for qw/update_state terminate global/;
-
-warns {
-    is([$one->to_tap()], [], "to_tap is an empty list by default");
-};
 
 done_testing;

t/modules/Event/Bail.t
@@ -1,8 +1,7 @@
-use Test::Sync -V1;
-
+use strict;
+use warnings;
+use Test::Sync::Tester;
 use Test::Sync::Event::Bail;
-
-use Test::Sync::Formatter::TAP qw/OUT_STD/;
 
 my $bail = Test::Sync::Event::Bail->new(
     debug => 'fake',
@@ -10,14 +9,6 @@ my $bail = Test::Sync::Event::Bail->new(
 );
 
 ok($bail->causes_fail, "balout always causes fail.");
-
-warns {
-    is(
-        [$bail->to_tap(1)],
-        [[OUT_STD, "Bail out!  evil\n" ]],
-        "Got tap"
-    );
-};
 
 is($bail->terminate, 255, "Bail will cause the test to exit.");
 is($bail->global, 1, "Bail is global, everything should bail");

t/modules/Event/Diag.t
@@ -1,43 +1,13 @@
-use Test::Sync -V1, Compare => ['-all'];
-
+use strict;
+use warnings;
+use Test::Sync::Tester;
 use Test::Sync::Event::Diag;
 use Test::Sync::DebugInfo;
-
-use Test::Sync::Formatter::TAP qw/OUT_TODO OUT_ERR/;
 
 my $diag = Test::Sync::Event::Diag->new(
     debug => Test::Sync::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__]),
     message => 'foo',
 );
-
-warns {
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_ERR, "# foo\n"]],
-        "Got tap"
-    );
-
-    $diag->set_message("foo\n");
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_ERR, "# foo\n"]],
-        "Only 1 newline"
-    );
-
-    $diag->debug->set_todo('todo');
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_TODO, "# foo\n"]],
-        "Got tap in todo"
-    );
-
-    $diag->set_message("foo\nbar\nbaz");
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_TODO, "# foo\n# bar\n# baz\n"]],
-        "All lines have proper prefix"
-    );
-};
 
 $diag = Test::Sync::Event::Diag->new(
     debug => Test::Sync::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__]),
@@ -52,13 +22,5 @@ $diag = Test::Sync::Event::Diag->new(
 );
 
 like($diag->message, qr/^HASH\(.*\)$/, "stringified the input value");
-
-warns {
-    $diag->set_message("");
-    is([$diag->to_tap], [], "no tap with an empty message");
-
-    $diag->set_message("\n");
-    is([$diag->to_tap], [[OUT_ERR, "\n"]], "newline on its own is unchanged");
-};
 
 done_testing;

t/modules/Event/Subtest.t
@@ -1,6 +1,7 @@
-use Test::Sync -V1;
+use strict;
+use warnings;
 
-use Test::Sync::Formatter::TAP qw/OUT_STD OUT_ERR/;
+use Test::Sync::Tester;
 use Test::Sync::Event::Subtest;
 my $st = 'Test::Sync::Event::Subtest';
 
@@ -12,113 +13,7 @@ my $one = $st->new(
     name      => 'foo',
 );
 
-isa_ok($one, $st, 'Test::Sync::Event::Ok');
-is($one->subevents, [], "subevents is an arrayref");
-
-warns {
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "ok 5 - foo {\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output"
-    );
-};
-
-$one->set_buffered(0);
-warns {
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "ok 5 - foo\n"],
-        ],
-        "Got Unbuffered TAP output"
-    );
-};
-
-$dbg = Test::Sync::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__, 'xxx']);
-$one = $st->new(
-    debug     => $dbg,
-    pass      => 0,
-    buffered  => 1,
-    name      => 'bar',
-    diag      => [ 'bar failed' ],
-    subevents => [
-        Test::Sync::Event::Ok->new(debug => $dbg, name => 'first',  pass => 1),
-        Test::Sync::Event::Ok->new(debug => $dbg, name => 'second', pass => 0, diag => ["second failed"]),
-        Test::Sync::Event::Ok->new(debug => $dbg, name => 'third',  pass => 1),
-
-        Test::Sync::Event::Diag->new(debug => $dbg, message => 'blah blah'),
-
-        Test::Sync::Event::Plan->new(debug => $dbg, max => 3),
-    ],
-);
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE};
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "not ok 5 - bar {\n"],
-            [OUT_ERR, "# bar failed\n"],
-            [OUT_STD, "    ok 1 - first\n"],
-            [OUT_STD, "    not ok 2 - second\n"],
-            [OUT_ERR, "    # second failed\n"],
-            [OUT_STD, "    ok 3 - third\n"],
-            [OUT_ERR, "    # blah blah\n"],
-            [OUT_STD, "    1..3\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output (non-verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE} = 1;
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "not ok 5 - bar {\n"],
-            [OUT_ERR, "    # bar failed\n"],
-            [OUT_STD, "    ok 1 - first\n"],
-            [OUT_STD, "    not ok 2 - second\n"],
-            [OUT_ERR, "    # second failed\n"],
-            [OUT_STD, "    ok 3 - third\n"],
-            [OUT_ERR, "    # blah blah\n"],
-            [OUT_STD, "    1..3\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output (verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE};
-    $one->set_buffered(0);
-    is(
-        [$one->to_tap(5)],
-        [
-            # In unbuffered TAP the subevents are rendered outside of this.
-            [OUT_STD, "not ok 5 - bar\n"],
-            [OUT_ERR, "# bar failed\n"],
-        ],
-        "Got Unbuffered TAP output (non-verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE} = 1;
-    $one->set_buffered(0);
-    is(
-        [$one->to_tap(5)],
-        [
-            # In unbuffered TAP the subevents are rendered outside of this.
-            [OUT_STD, "not ok 5 - bar\n"],
-            [OUT_ERR, "# bar failed\n"],
-        ],
-        "Got Unbuffered TAP output (verbose)"
-    );
-};
+ok($one->isa('Test::Sync::Event::Ok'), "Inherit from Ok");
+is_deeply($one->subevents, [], "subevents is an arrayref");
 
 done_testing;

t/modules/HashBase.t
@@ -1,4 +1,6 @@
-use Test::Sync -V1;
+use strict;
+use warnings;
+use Test::Sync::Tester;
 
 BEGIN {
     $INC{'My/HBase.pm'} = __FILE__;
@@ -8,7 +10,7 @@ BEGIN {
         accessors => [qw/foo bar baz/],
     );
 
-    use Test::Sync -V1;
+    use Test::Sync::Tester;
     is(FOO, 'foo', "FOO CONSTANT");
     is(BAR, 'bar', "BAR CONSTANT");
     is(BAZ, 'baz', "BAZ CONSTANT");
@@ -19,58 +21,13 @@ BEGIN {
     use base 'My::HBase';
     use Test::Sync::HashBase accessors => [qw/apple pear/];
 
-    use Test::Sync -V1;
+    use Test::Sync::Tester;
     is(FOO,   'foo',   "FOO CONSTANT");
     is(BAR,   'bar',   "BAR CONSTANT");
     is(BAZ,   'baz',   "BAZ CONSTANT");
     is(APPLE, 'apple', "APPLE CONSTANT");
     is(PEAR,  'pear',  "PEAR CONSTANT");
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Sync::HashBase->import( base => 'foobarbaz' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'foobarbaz' is not a HashBase class/, "Expected error");
 }
-
-BEGIN {
-    package My::HBaseSubDep;
-    BEGIN {
-        my $warning;
-        local $SIG{__WARN__} = sub { $warning = shift };
-        Test::Sync::HashBase->import(
-            accessors => [qw/apple pear/],
-            base      => 'My::HBase',
-        );
-        main::like($warning, qr/'base' argument to HashBase is deprecated\./, "got import deprecation warning");
-    }
-
-    use Test::Sync -V1;
-    is(FOO,   'foo',   "FOO CONSTANT");
-    is(BAR,   'bar',   "BAR CONSTANT");
-    is(BAZ,   'baz',   "BAZ CONSTANT");
-    is(APPLE, 'apple', "APPLE CONSTANT");
-    is(PEAR,  'pear',  "PEAR CONSTANT");
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Sync::HashBase->import( base => 'foobarbaz' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'foobarbaz' is not a HashBase class/, "Expected error");
-}
-
-{
-    package Consumer;
-    use Test::Sync -V1;
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Sync::HashBase->import( base => 'Fake::Thing' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'Fake::Thing' is not a HashBase class/, "Expected error");
-}
-
-isa_ok('My::HBaseSub', 'My::HBase');
 
 my $one = My::HBase->new(foo => 'a', bar => 'b', baz => 'c');
 is($one->foo, 'a', "Accessor");
@@ -80,7 +37,7 @@ $one->set_foo('x');
 is($one->foo, 'x', "Accessor set");
 $one->set_foo(undef);
 
-is(
+is_deeply(
     $one,
     {
         foo => undef,
@@ -91,7 +48,7 @@ is(
 );
 
 $one->clear_foo;
-is(
+is_deeply(
     $one,
     {
         bar => 'b',
@@ -100,33 +57,32 @@ is(
     'hash'
 );
 
-
 my $obj = bless {}, 'FAKE';
 
 my $accessor = Test::Sync::HashBase->gen_accessor('foo');
 my $getter   = Test::Sync::HashBase->gen_getter('foo');
 my $setter   = Test::Sync::HashBase->gen_setter('foo');
 
-is($obj, {}, "nothing set");
+is_deeply($obj, {}, "nothing set");
 
 is($obj->$accessor(), undef, "nothing set");
 is($obj->$accessor('foo'), 'foo', "set value");
 is($obj->$accessor(), 'foo', "was set");
 
-is($obj, {foo => 'foo'}, "set");
+is_deeply($obj, {foo => 'foo'}, "set");
 
 is($obj->$getter(), 'foo', "got the value");
 is($obj->$getter(), 'foo', "got the value again");
 
-is($obj, {foo => 'foo'}, "no change");
+is_deeply($obj, {foo => 'foo'}, "no change");
 
 is( $obj->$setter, undef, "set to nothing" );
-is($obj, {foo => undef}, "nothing");
+is_deeply($obj, {foo => undef}, "nothing");
 is( $obj->$setter('foo'), 'foo', "set it again" );
-is($obj, {foo => 'foo'}, "is set");
+is_deeply($obj, {foo => 'foo'}, "is set");
 is($obj->$getter(), 'foo', "got the value");
 is($obj->$accessor('foo'), 'foo', "get via accessor");
 
-is($obj, {foo => 'foo'}, "no change");
+is_deeply($obj, {foo => 'foo'}, "no change");
 
 done_testing;

t/modules/State.t
@@ -1,10 +1,10 @@
-use Test::Sync -V1;
+use strict;
+use warnings;
 
+use Test::Sync::Tester;
 use Test::Sync::State;
 
 my $state = 'Test::Sync::State'->new;
-
-can_ok($state, qw/count failed ended is_passing plan/);
 
 is($state->count, 0, "count starts at 0");
 is($state->failed, 0, "failed starts at 0");

Test-More/test-more

t/behavior/Taint.t
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl -T
-use Test::Stream Core;
-
-plan(2);
+use Test::Stream::Tester;
 
 ok(1);
 ok(1);

t/modules/DebugInfo.t
@@ -1,16 +1,17 @@
-use Test::Stream -V1, -Tester;
+use strict;
+use warnings;
+use Test::Stream::Tester;
 use Test::Stream::DebugInfo;
 
 like(
-    dies { 'Test::Stream::DebugInfo'->new() },
+    exception { 'Test::Stream::DebugInfo'->new() },
     qr/Frame is required/,
     "got error"
 );
 
 my $one = 'Test::Stream::DebugInfo'->new(frame => ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo']);
-isa_ok($one, 'Test::Stream::DebugInfo');
-is($one->frame,  ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got frame");
-is([$one->call], ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got call");
+is_deeply($one->frame,  ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got frame");
+is_deeply([$one->call], ['Foo::Bar', 'foo.t', 5, 'Foo::Bar::foo'], "Got call");
 is($one->package, 'Foo::Bar',      "Got package");
 is($one->file,    'foo.t',         "Got file");
 is($one->line,    5,               "Got line");
@@ -21,45 +22,20 @@ $one->set_detail("yo momma");
 is($one->trace, "yo momma", "got detail for trace");
 $one->set_detail(undef);
 
-ok(!eval { $one->throw('I died'); 1 }, "threw exception");
-is($@, "I died at foo.t line 5.\n", "got exception");
+is(
+    exception { $one->throw('I died') },
+    "I died at foo.t line 5.\n",
+    "got exception"
+);
 
-my @warnings;
-{
-    local $SIG{__WARN__} = sub { push @warnings => @_ };
-    $one->alert('I cried');
-}
-
-like(
-    warns { $one->alert('I cried') },
-    [ qr/I cried at foo\.t line 5/ ],
+is_deeply(
+    warnings { $one->alert('I cried') },
+    [ "I cried at foo.t line 5.\n" ],
     "alter() warns"
 );
 
 my $snap = $one->snapshot;
-is($snap, $one, "identical");
+is_deeply($snap, $one, "identical");
 ok($snap != $one, "Not the same instance");
-
-
-ok(!$one->_no_diag, "yes diag");
-ok(!$one->_no_fail, "yes fail");
-
-$one->_set_parent_todo(1);
-ok($one->_no_diag, "no diag");
-ok(!$one->_no_fail, "yes fail");
-
-$one->_set_parent_todo(0);
-$one->_set_todo(1);
-ok($one->_no_diag, "no diag");
-ok($one->_no_fail, "no fail");
-
-$one->_set_todo(undef);
-like(
-    warning { $one->set_skip(1) },
-    qr/Use of 'skip' attribute for DebugInfo is deprecated/,
-    "Got expected warning for deprecated 'skip' attribute"
-);
-ok($one->_no_diag, "no diag");
-ok($one->_no_fail, "no fail");
 
 done_testing;

t/modules/Event.t
@@ -1,8 +1,8 @@
-use Test::Stream -V1;
+use strict;
+use warnings;
+use Test::Stream::Tester;
 
 use Test::Stream::Event();
-
-can_ok('Test::Stream::Event', qw/debug nested/);
 
 my $ok = eval { Test::Stream::Event->new(); 1 };
 my $err = $@;
@@ -16,17 +16,12 @@ like($err, qr/No debug info provided/, "Need debug info");
     use Test::Stream::HashBase accessors => [qw/foo bar baz/];
 }
 
-can_ok('My::MockEvent', qw/foo bar baz/);
-isa_ok('My::MockEvent', 'Test::Stream::Event');
+ok(My::MockEvent->can($_), "Added $_ accessor") for qw/foo bar baz/;
 
 my $one = My::MockEvent->new(debug => 'fake');
 
 ok(!$one->causes_fail, "Events do not cause failures by default");
 
 ok(!$one->$_, "$_ is false by default") for qw/update_state terminate global/;
-
-warns {
-    is([$one->to_tap()], [], "to_tap is an empty list by default");
-};
 
 done_testing;

t/modules/Event/Bail.t
@@ -1,8 +1,7 @@
-use Test::Stream -V1;
-
+use strict;
+use warnings;
+use Test::Stream::Tester;
 use Test::Stream::Event::Bail;
-
-use Test::Stream::Formatter::TAP qw/OUT_STD/;
 
 my $bail = Test::Stream::Event::Bail->new(
     debug => 'fake',
@@ -10,14 +9,6 @@ my $bail = Test::Stream::Event::Bail->new(
 );
 
 ok($bail->causes_fail, "balout always causes fail.");
-
-warns {
-    is(
-        [$bail->to_tap(1)],
-        [[OUT_STD, "Bail out!  evil\n" ]],
-        "Got tap"
-    );
-};
 
 is($bail->terminate, 255, "Bail will cause the test to exit.");
 is($bail->global, 1, "Bail is global, everything should bail");

t/modules/Event/Diag.t
@@ -1,43 +1,13 @@
-use Test::Stream -V1, Compare => ['-all'];
-
+use strict;
+use warnings;
+use Test::Stream::Tester;
 use Test::Stream::Event::Diag;
 use Test::Stream::DebugInfo;
-
-use Test::Stream::Formatter::TAP qw/OUT_TODO OUT_ERR/;
 
 my $diag = Test::Stream::Event::Diag->new(
     debug => Test::Stream::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__]),
     message => 'foo',
 );
-
-warns {
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_ERR, "# foo\n"]],
-        "Got tap"
-    );
-
-    $diag->set_message("foo\n");
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_ERR, "# foo\n"]],
-        "Only 1 newline"
-    );
-
-    $diag->debug->set_todo('todo');
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_TODO, "# foo\n"]],
-        "Got tap in todo"
-    );
-
-    $diag->set_message("foo\nbar\nbaz");
-    is(
-        [$diag->to_tap(1)],
-        [[OUT_TODO, "# foo\n# bar\n# baz\n"]],
-        "All lines have proper prefix"
-    );
-};
 
 $diag = Test::Stream::Event::Diag->new(
     debug => Test::Stream::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__]),
@@ -52,13 +22,5 @@ $diag = Test::Stream::Event::Diag->new(
 );
 
 like($diag->message, qr/^HASH\(.*\)$/, "stringified the input value");
-
-warns {
-    $diag->set_message("");
-    is([$diag->to_tap], [], "no tap with an empty message");
-
-    $diag->set_message("\n");
-    is([$diag->to_tap], [[OUT_ERR, "\n"]], "newline on its own is unchanged");
-};
 
 done_testing;

t/modules/Event/Subtest.t
@@ -1,6 +1,7 @@
-use Test::Stream -V1;
+use strict;
+use warnings;
 
-use Test::Stream::Formatter::TAP qw/OUT_STD OUT_ERR/;
+use Test::Stream::Tester;
 use Test::Stream::Event::Subtest;
 my $st = 'Test::Stream::Event::Subtest';
 
@@ -12,113 +13,7 @@ my $one = $st->new(
     name      => 'foo',
 );
 
-isa_ok($one, $st, 'Test::Stream::Event::Ok');
-is($one->subevents, [], "subevents is an arrayref");
-
-warns {
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "ok 5 - foo {\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output"
-    );
-};
-
-$one->set_buffered(0);
-warns {
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "ok 5 - foo\n"],
-        ],
-        "Got Unbuffered TAP output"
-    );
-};
-
-$dbg = Test::Stream::DebugInfo->new(frame => [__PACKAGE__, __FILE__, __LINE__, 'xxx']);
-$one = $st->new(
-    debug     => $dbg,
-    pass      => 0,
-    buffered  => 1,
-    name      => 'bar',
-    diag      => [ 'bar failed' ],
-    subevents => [
-        Test::Stream::Event::Ok->new(debug => $dbg, name => 'first',  pass => 1),
-        Test::Stream::Event::Ok->new(debug => $dbg, name => 'second', pass => 0, diag => ["second failed"]),
-        Test::Stream::Event::Ok->new(debug => $dbg, name => 'third',  pass => 1),
-
-        Test::Stream::Event::Diag->new(debug => $dbg, message => 'blah blah'),
-
-        Test::Stream::Event::Plan->new(debug => $dbg, max => 3),
-    ],
-);
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE};
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "not ok 5 - bar {\n"],
-            [OUT_ERR, "# bar failed\n"],
-            [OUT_STD, "    ok 1 - first\n"],
-            [OUT_STD, "    not ok 2 - second\n"],
-            [OUT_ERR, "    # second failed\n"],
-            [OUT_STD, "    ok 3 - third\n"],
-            [OUT_ERR, "    # blah blah\n"],
-            [OUT_STD, "    1..3\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output (non-verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE} = 1;
-    is(
-        [$one->to_tap(5)],
-        [
-            [OUT_STD, "not ok 5 - bar {\n"],
-            [OUT_ERR, "    # bar failed\n"],
-            [OUT_STD, "    ok 1 - first\n"],
-            [OUT_STD, "    not ok 2 - second\n"],
-            [OUT_ERR, "    # second failed\n"],
-            [OUT_STD, "    ok 3 - third\n"],
-            [OUT_ERR, "    # blah blah\n"],
-            [OUT_STD, "    1..3\n"],
-            [OUT_STD, "}\n"],
-        ],
-        "Got Buffered TAP output (verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE};
-    $one->set_buffered(0);
-    is(
-        [$one->to_tap(5)],
-        [
-            # In unbuffered TAP the subevents are rendered outside of this.
-            [OUT_STD, "not ok 5 - bar\n"],
-            [OUT_ERR, "# bar failed\n"],
-        ],
-        "Got Unbuffered TAP output (non-verbose)"
-    );
-};
-
-warns {
-    local $ENV{HARNESS_IS_VERBOSE} = 1;
-    $one->set_buffered(0);
-    is(
-        [$one->to_tap(5)],
-        [
-            # In unbuffered TAP the subevents are rendered outside of this.
-            [OUT_STD, "not ok 5 - bar\n"],
-            [OUT_ERR, "# bar failed\n"],
-        ],
-        "Got Unbuffered TAP output (verbose)"
-    );
-};
+ok($one->isa('Test::Stream::Event::Ok'), "Inherit from Ok");
+is_deeply($one->subevents, [], "subevents is an arrayref");
 
 done_testing;

t/modules/HashBase.t
@@ -1,4 +1,6 @@
-use Test::Stream -V1;
+use strict;
+use warnings;
+use Test::Stream::Tester;
 
 BEGIN {
     $INC{'My/HBase.pm'} = __FILE__;
@@ -8,7 +10,7 @@ BEGIN {
         accessors => [qw/foo bar baz/],
     );
 
-    use Test::Stream -V1;
+    use Test::Stream::Tester;
     is(FOO, 'foo', "FOO CONSTANT");
     is(BAR, 'bar', "BAR CONSTANT");
     is(BAZ, 'baz', "BAZ CONSTANT");
@@ -19,58 +21,13 @@ BEGIN {
     use base 'My::HBase';
     use Test::Stream::HashBase accessors => [qw/apple pear/];
 
-    use Test::Stream -V1;
+    use Test::Stream::Tester;
     is(FOO,   'foo',   "FOO CONSTANT");
     is(BAR,   'bar',   "BAR CONSTANT");
     is(BAZ,   'baz',   "BAZ CONSTANT");
     is(APPLE, 'apple', "APPLE CONSTANT");
     is(PEAR,  'pear',  "PEAR CONSTANT");
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Stream::HashBase->import( base => 'foobarbaz' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'foobarbaz' is not a HashBase class/, "Expected error");
 }
-
-BEGIN {
-    package My::HBaseSubDep;
-    BEGIN {
-        my $warning;
-        local $SIG{__WARN__} = sub { $warning = shift };
-        Test::Stream::HashBase->import(
-            accessors => [qw/apple pear/],
-            base      => 'My::HBase',
-        );
-        main::like($warning, qr/'base' argument to HashBase is deprecated\./, "got import deprecation warning");
-    }
-
-    use Test::Stream -V1;
-    is(FOO,   'foo',   "FOO CONSTANT");
-    is(BAR,   'bar',   "BAR CONSTANT");
-    is(BAZ,   'baz',   "BAZ CONSTANT");
-    is(APPLE, 'apple', "APPLE CONSTANT");
-    is(PEAR,  'pear',  "PEAR CONSTANT");
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Stream::HashBase->import( base => 'foobarbaz' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'foobarbaz' is not a HashBase class/, "Expected error");
-}
-
-{
-    package Consumer;
-    use Test::Stream -V1;
-
-    local $SIG{__WARN__} = sub { 1 };
-    my $bad = eval { Test::Stream::HashBase->import( base => 'Fake::Thing' ); 1 };
-    my $error = $@;
-    ok(!$bad, "Threw exception");
-    like($error, qr/Base class 'Fake::Thing' is not a HashBase class/, "Expected error");
-}
-
-isa_ok('My::HBaseSub', 'My::HBase');
 
 my $one = My::HBase->new(foo => 'a', bar => 'b', baz => 'c');
 is($one->foo, 'a', "Accessor");
@@ -80,7 +37,7 @@ $one->set_foo('x');
 is($one->foo, 'x', "Accessor set");
 $one->set_foo(undef);
 
-is(
+is_deeply(
     $one,
     {
         foo => undef,
@@ -91,7 +48,7 @@ is(
 );
 
 $one->clear_foo;
-is(
+is_deeply(
     $one,
     {
         bar => 'b',
@@ -100,33 +57,32 @@ is(
     'hash'
 );
 
-
 my $obj = bless {}, 'FAKE';
 
 my $accessor = Test::Stream::HashBase->gen_accessor('foo');
 my $getter   = Test::Stream::HashBase->gen_getter('foo');
 my $setter   = Test::Stream::HashBase->gen_setter('foo');
 
-is($obj, {}, "nothing set");
+is_deeply($obj, {}, "nothing set");
 
 is($obj->$accessor(), undef, "nothing set");
 is($obj->$accessor('foo'), 'foo', "set value");
 is($obj->$accessor(), 'foo', "was set");
 
-is($obj, {foo => 'foo'}, "set");
+is_deeply($obj, {foo => 'foo'}, "set");
 
 is($obj->$getter(), 'foo', "got the value");
 is($obj->$getter(), 'foo', "got the value again");
 
-is($obj, {foo => 'foo'}, "no change");
+is_deeply($obj, {foo => 'foo'}, "no change");
 
 is( $obj->$setter, undef, "set to nothing" );
-is($obj, {foo => undef}, "nothing");
+is_deeply($obj, {foo => undef}, "nothing");
 is( $obj->$setter('foo'), 'foo', "set it again" );
-is($obj, {foo => 'foo'}, "is set");
+is_deeply($obj, {foo => 'foo'}, "is set");
 is($obj->$getter(), 'foo', "got the value");
 is($obj->$accessor('foo'), 'foo', "get via accessor");
 
-is($obj, {foo => 'foo'}, "no change");
+is_deeply($obj, {foo => 'foo'}, "no change");
 
 done_testing;

t/modules/State.t
@@ -1,10 +1,10 @@
-use Test::Stream -V1;
+use strict;
+use warnings;
 
+use Test::Stream::Tester;
 use Test::Stream::State;
 
 my $state = 'Test::Stream::State'->new;
-
-can_ok($state, qw/count failed ended is_passing plan/);
 
 is($state->count, 0, "count starts at 0");
 is($state->failed, 0, "failed starts at 0");