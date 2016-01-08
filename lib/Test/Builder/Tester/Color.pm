6ddcf4405f25e84bc9d91b36a20b5ebe9b6d90eb
Chad Granum <exodist7@gmail.com>
2016-01-07 19:29:11 -0800

    Bump minimum Test2 version

--- lib/Test/Builder/Tester/Color.pm
+++ lib/Test/Builder/Tester/Color.pm
@@ -1,7 +1,7 @@
 package Test::Builder::Tester::Color;
 
 use strict;
-our $VERSION = '1.302013_006';
+our $VERSION = '1.302013_007';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 require Test::Builder::Tester;
--- lib/ok.pm
+++ lib/ok.pm
@@ -1,5 +1,5 @@
 package ok;
-$ok::VERSION = '1.302013_006';
+$ok::VERSION = '1.302013_007';
 
 use strict;
 use Test::More ();
