169f96308bdca1a652116ac43ac1762ef19dbf79
Chad Granum <exodist7@gmail.com>
2016-01-14 21:55:20 -0800

    Version Bump

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '1.302013_010';
+our $VERSION = '1.302013_011';
 $VERSION = eval $VERSION;    ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 BEGIN {
--- lib/ok.pm
+++ lib/ok.pm
@@ -1,5 +1,5 @@
 package ok;
-$ok::VERSION = '1.302013_010';
+$ok::VERSION = '1.302013_011';
 
 use strict;
 use Test::More ();
