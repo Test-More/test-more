16320e1b6d6355397bc89f88eea507daf66f24e5
Chad Granum <exodist7@gmail.com>
2016-03-18 17:53:02 -0700

    Post-release version bump

--- lib/Test2/AsyncSubtest.pm
+++ lib/Test2/AsyncSubtest.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use Test2::IPC;
 
-our $VERSION = '0.000012';
+our $VERSION = '0.000013';
 
 our @CARP_NOT = qw/Test2::Util::HashBase/;
 
--- lib/Test2/Tools/AsyncSubtest.pm
+++ lib/Test2/Tools/AsyncSubtest.pm
@@ -2,7 +2,7 @@ package Test2::Tools::AsyncSubtest;
 use strict;
 use warnings;
 
-our $VERSION = '0.000012';
+our $VERSION = '0.000013';
 
 use Test2::IPC;
 use Test2::AsyncSubtest;
