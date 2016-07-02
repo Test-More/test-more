29ab1f4d768cf0bfa8d6076fbf3a73afd35dfd54
Chad Granum <exodist7@gmail.com>
2016-07-01 17:33:14 -0700

    Version Bump

--- lib/Test2/Workflow.pm
+++ lib/Test2/Workflow.pm
@@ -2,7 +2,7 @@ package Test2::Workflow;
 use strict;
 use warnings;
 
-our $VERSION = "0.000012";
+our $VERSION = "0.000013";
 
 our @EXPORT_OK = qw/parse_args current_build build root_build init_root/;
 use base 'Exporter';
