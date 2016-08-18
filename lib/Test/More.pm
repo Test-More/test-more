f1220f352c20491408ab66981260d48ca7fdd038
Chad Granum <exodist7@gmail.com>
2016-08-17 21:22:41 -0700

    Version Bump

--- lib/Test/More.pm
+++ lib/Test/More.pm
@@ -17,7 +17,7 @@ sub _carp {
     return warn @_, " at $file line $line\n";
 }
 
-our $VERSION = '1.302052';
+our $VERSION = '1.302053';
 
 use Test::Builder::Module;
 our @ISA    = qw(Test::Builder::Module);
--- lib/Test2/Formatter/TAP.pm
+++ lib/Test2/Formatter/TAP.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 require PerlIO;
 
-our $VERSION = '1.302052';
+our $VERSION = '1.302053';
 
 
 use Test2::Util::HashBase qw{
