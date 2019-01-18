5e4257bbc64382f79f877337907be5827a92862e
Chad Granum <exodist7@gmail.com>
2019-01-18 13:43:21 -0800

    Fix test broken by latest Test-Simple
    Fixes #175
    
    This was a flaw in the Test2::Tools::Tester which made some bad
    assumptions, fixed now.

--- lib/Test2/Tools/Tester.pm
+++ lib/Test2/Tools/Tester.pm
@@ -19,9 +19,12 @@ our @EXPORT_OK = qw{
 my %TYPES;
 for my $class (__PACKAGE__->plugins) {
     my $type = $class;
-    $type =~ s/^.*::EventFacet:://g;
+    $type =~ s/^Test2::EventFacet:://g;
 
-    my $key = $class->facet_key || lc($type);
+    next unless $class->isa('Test2::EventFacet');
+    my $key;
+    $key = $class->facet_key if $class->can('facet_key');
+    $key = lc($type) unless defined $key;
 
     $TYPES{$type}     = $class;
     $TYPES{lc($type)} = $class;
