4c1bdd27e7f7694cbdfe4ec83b79b1cf791b4bc5
Chad Granum <exodist7@gmail.com>
2019-01-28 21:13:15 -0800

    Remove SHM optimization
    This was leaky and caused a lot of bugs. The benefit is no longer clear,
    it does not appear to produce any noticable performance enhancements on
    modern systems.
    
    Fixes #815
    Fixes #823

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -27,7 +27,6 @@ BEGIN {
         Test2::IPC::Driver::Files->import;
         Test2::API::test2_ipc_enable_polling();
         Test2::API::test2_no_wait(1);
-        Test2::API::test2_ipc_enable_shm();
     }
 }
 
@@ -1760,7 +1759,6 @@ sub coordinate_forks {
     my $ipc = Test2::IPC::apply_ipc($self->{Stack});
     $ipc->set_no_fatal(1);
     Test2::API::test2_no_wait(1);
-    Test2::API::test2_ipc_enable_shm();
 }
 
 sub no_log_results { $_[0]->{no_log_results} = 1 }
