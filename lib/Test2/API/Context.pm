5191adace7755919ced0a0afb67a1c99badfd05a
Chad Granum <exodist7@gmail.com>
2019-05-15 07:53:38 -0700

    Better disgnostics when a context is destroyed unexpectedly
    Add a trace to the code that caused the destruction.
    
    fixes #833

--- lib/Test2/API/Context.pm
+++ lib/Test2/API/Context.pm
@@ -71,6 +71,8 @@ sub DESTROY {
         # show the warning about using eq.
         no warnings 'uninitialized';
         if($self->{+EVAL_ERROR} eq $@ && $hub->is_local) {
+            require Carp;
+            my $mess = Carp::longmess("Context destroyed");
             my $frame = $self->{+_IS_SPAWN} || $self->{+TRACE}->frame;
             warn <<"            EOT";
 A context appears to have been destroyed without first calling release().
@@ -86,6 +88,10 @@ release():
   File: $frame->[1]
   Line: $frame->[2]
   Tool: $frame->[3]
+
+Here is a trace to the code that caused the context to be destroyed, this could
+be an exit(), a goto, or simply the end of a scope:
+$mess
 
 Cleaning up the CONTEXT stack...
             EOT
