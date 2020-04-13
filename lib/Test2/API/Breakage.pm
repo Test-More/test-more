bff4d5560f7090366c6eb4dc0a5cbcd1145511ea
Chad Granum <exodist7@gmail.com>
2020-04-13 11:25:54 -0700

    More data when breakage test fails
    Fixes #836

--- lib/Test2/API/Breakage.pm
+++ lib/Test2/API/Breakage.pm
@@ -73,7 +73,9 @@ sub report {
         next unless $INC{$file} || ($require && eval { require $file; 1 });
         my $want = $suggest{$mod};
         next if eval { $mod->VERSION($want); 1 };
-        push @warn => " * Module '$mod' is outdated, we recommend updating above $want.";
+        my $error = $@;
+        chomp $error;
+        push @warn => " * Module '$mod' is outdated, we recommed updating above $want. error was: '$error'; INC is $INC{$file}";
     }
 
     for my $mod (keys %required) {
