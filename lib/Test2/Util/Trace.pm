937e25a2db049299ac6e79274b3e02ef2ab5ddbf
Todd Rinaldo <toddr@cpan.org>
2020-08-07 13:19:11 -0500

    use strict in Test2::Util::Trace

--- lib/Test2/Util/Trace.pm
+++ lib/Test2/Util/Trace.pm
@@ -1,6 +1,10 @@
 package Test2::Util::Trace;
 require Test2::EventFacet::Trace;
-@ISA = ('Test2::EventFacet::Trace');
+
+use warnings;
+use strict;
+
+our @ISA = ('Test2::EventFacet::Trace');
 
 our $VERSION = '1.302178';
 
