4f706e0514fbe11840699534668389373cf2da0c
Chad Granum <exodist7@gmail.com>
2021-05-19 11:08:55 -0700

    Automated Version Bump

--- lib/Test2/API.pm
+++ lib/Test2/API.pm
@@ -9,7 +9,7 @@ BEGIN {
     $ENV{TEST2_ACTIVE} = 1;
 }
 
-our $VERSION = '1.302185';
+our $VERSION = '1.302186';
 
 
 my $INST;
--- lib/Test2/Util.pm
+++ lib/Test2/Util.pm
@@ -2,7 +2,7 @@ package Test2::Util;
 use strict;
 use warnings;
 
-our $VERSION = '1.302185';
+our $VERSION = '1.302186';
 
 use POSIX();
 use Config qw/%Config/;
