053446ff3a6229883b67c9bd41afdbae806fca49
Chad Granum <exodist7@gmail.com>
2022-02-24 21:23:23 -0800

    Automated Version Bump

--- lib/Test/Builder.pm
+++ lib/Test/Builder.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '1.302189';
+our $VERSION = '1.302190';
 
 BEGIN {
     if( $] < 5.008 ) {
--- lib/Test2/Util/HashBase.pm
+++ lib/Test2/Util/HashBase.pm
@@ -2,7 +2,7 @@ package Test2::Util::HashBase;
 use strict;
 use warnings;
 
-our $VERSION = '1.302189';
+our $VERSION = '1.302190';
 
 #################################################################
 #                                                               #
